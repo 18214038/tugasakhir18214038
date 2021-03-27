44de5023c7e143b870cca0b08a58b9dcdd049f03
18214038 <81459664+18214038@users.noreply.github.com>
2021-03-27 13:42:59 +0700

    Delete take.php

--- take.php
+++
@@ -1,9 +0,0 @@
-<?php
-  include("config.php"); //Just database initialization
-  session_start();
-  
-  $sql = "insert into enrollment
-          values ($_POST[studentId],$_POST[courseId])";
-          
-  mysqli_query($db,$sql);
-?>
